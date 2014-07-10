71d504a0aa30b70d0dd9222b4ac0b96f2a55dd44
Chris <christopher.w.finlayson@gmail.com>
2014-07-10 00:43:03 -0400

    user sign out test completed

--- spec/features/user_sign_out_spec.rb
+++ spec/features/user_sign_out_spec.rb
@@ -1,2 +1,21 @@
 require 'rails_helper'
 
+feature "user signs out", %q{
+  As a User
+  I want to sign out
+  So that other people can't use my account
+} do
+
+  scenario "user signs out" do
+    user = FactoryGirl.create(:user)
+
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_on "Sign in"
+
+    click_on "Sign Out"
+
+    expect(page).to have_content "Signed out successfully."
+  end
+end
