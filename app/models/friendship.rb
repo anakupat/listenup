fbcdb0e2014f075281a23d15cab4d791896b6b40
Chris <christopher.w.finlayson@gmail.com>
2014-07-11 16:53:35 -0400

    cannot recreate friendships that already exists both ways

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,17 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => "User"
 
+  validates :user_id, uniqueness: { scope: :friend_id}
+
+
+  validate :uniquiness_of_friendship
+
+  def uniquiness_of_friendship
+
+    unless Friendship.where(user_id: self.user_id, friend_id: self.friend_id).empty? &&
+       Friendship.where(user_id: self.friend_id, friend_id: self.user_id).empty?
+
+       errors[:base] << ("Friendship already exists")
+    end
+  end
 end
