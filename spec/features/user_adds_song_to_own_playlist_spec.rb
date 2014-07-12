4a4ab8a002f7c82574ab3f97ddab1b28bc7debf3
Chris <christopher.w.finlayson@gmail.com>
2014-07-12 14:55:20 -0400

    started to write test for adding to own playlist but does not recognize @friendships

---
+++ spec/features/user_adds_song_to_own_playlist_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature "user searches song and adds to their own playlist", %q{
+  As a User
+  I want to search for songs and add to my playlist
+} do
+
+  # Acceptance Criteria
+  #
+  # - I must search for a song or artist name
+  # - I am redirected to search results page
+  # - I see Spotify iframes of potenial songs that match my search
+  # - I select song and add to my playlist
+
+  scenario "user searches for song and adds to their own playlist" do
+    visit root_path
+    user1 = FactoryGirl.create(:user)
+    user2 = FactoryGirl.create(:user)
+
+    Friendship.create(user_id: user1.id, friend_id: user2.id)
+
+    sign_in_as(user1)
+
+    fill_in "song, artist, album..", with: "Elton John"
+    click_on "search!"
+
+
+    within('.song-1') do
+     click_on "add to my playlist"
+    end
+
+    visit "/users/username1"
+
+    expect(page).to have_content "username1"
+    expect(page).to have_content "added by: username1"
+
+  end
+end
