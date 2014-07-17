fd65fa57b7edbf0461c9f32c85875652874acf92
Chris <christopher.w.finlayson@gmail.com>
2014-07-17 15:26:37 -0400

    removed unneed controller

--- app/controllers/playlists_controller.rb
+++
@@ -1,8 +0,0 @@
-class PlaylistsController < ApplicationController
-
-  # def show
-  #   @user = User.find_by_permalink(params[:id])
-  #   @params_id = @user.id
-  #   @songs = Song.where(user_id:  @params_id)
-  # end
-end
