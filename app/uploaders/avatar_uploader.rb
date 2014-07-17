3ffe4cfef0af43dc2b1b4fc78e6ef36de664b6b8
Chris <christopher.w.finlayson@gmail.com>
2014-07-17 11:12:58 -0400

    updating avatar upload for development

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,7 +9,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   process :resize_to_fit => [200, 200]
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
