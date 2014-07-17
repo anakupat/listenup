9b8fff40bc617c12e0bc9660c6a921c2e2c09155
Chris <christopher.w.finlayson@gmail.com>
2014-07-17 15:22:27 -0400

    validation to Song model that song can only be added once to a user playlist

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :user
 
   validates :user_id, :added_by, :song_uri, :song_name, presence: true
+  validates :song_name, uniqueness: { scope: :user_id,
+  message: "song already on playlist!" }
 end
