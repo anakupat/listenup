d81f85a21dc964fc2f9f2dfeb3f20da83e15f65e
Chris <christopher.w.finlayson@gmail.com>
2014-07-17 10:09:42 -0400

    validations in Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,17 +2,15 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
-  validates :user_id, uniqueness: { scope: :friend_id}
-
-
+  validates :user_id, :friend_id, presence: true
   validate :uniquiness_of_friendship
 
   def uniquiness_of_friendship
 
     unless Friendship.where(user_id: self.user_id, friend_id: self.friend_id).empty? &&
-       Friendship.where(user_id: self.friend_id, friend_id: self.user_id).empty?
+           Friendship.where(user_id: self.friend_id, friend_id: self.user_id).empty?
 
-       errors[:base] << ("Friendship already exists")
+      errors[:base] << ("Friendship already exists")
     end
   end
 end
